9acad082078b44c3245f6c4f77dff8c110b0256d
Linan <3383915327@qq.com>
2024-07-18 22:45:27 +0800

    抄了官方文档，能用

--- User/PWM.h
+++ User/PWM.h
@@ -5,8 +5,26 @@
 #ifndef L103PRO_USER_PWM_H_
 #define L103PRO_USER_PWM_H_
 #include "debug.h"
+#include "ch32l103.h"
+#define CODE_1  7
+#define CODE_0 3
+#define RESET_LEN    (60)
+#define  Pixel_NUM 8
+#define COLOR_BUFFER_LEN (((Pixel_NUM)*(3*8))+RESET_LEN)
+#define MAX_STEP (200)
+#define LIST_SIZE(list) (sizeof(list)/sizeof(list[0]))
+#define hex2rgb(c) (((c)>>16)&0xff),(((c)>>8)&0xff),((c)&0xff)
+
+
 void TIM2_PWMOut_Init(u16 arr, u16 psc, u16 ccp);
-void TIM2_DMA_Init(DMA_Channel_TypeDef *DMA_CHx, u32 ppadr, u32 memadr, u16 bufsize);
-void WS_Send(uint8_t *color,uint16_t len);
+void TIM2_DMA_Init(DMA_Channel_TypeDef *DMA_CHx, u32 ppadr, u16 bufsize);
+void WS_Send(uint8_t  *color,uint16_t len);
+void setPixelColor(uint16_t id, uint8_t r, uint8_t g, uint8_t b);
+void w2812_sync();
+uint32_t interpolateColors(uint32_t color1, uint32_t color2, uint8_t step);
+void led_example_0(void);
+void led_example_2(void);
+
+
 
 #endif //L103PRO_USER_PWM_H_
--- User/main.c
+++ User/main.c
@@ -1,20 +1,14 @@
-/*
- *I2C1_SCL(PB10)\I2C1_SDA(PB11).
- */
 
-#include <string.h>
 #include "debug.h"
 #include "PWM.h"
 #define Address_8bit  0
 #define Address_16bit  1
-/* CH1CVR register Definition */
 #define TIM2_CH2CVR_ADDRESS    0x40000038
 
 /* Private variables */
 
 #define Address_Lenth   Address_8bit
-//#define Address_Lenth   Address_16bit
-
+extern uint16_t color_buf[COLOR_BUFFER_LEN];
 
 /*********************************************************************
  * @fn      main
@@ -29,7 +23,6 @@ int main(void) {
     SystemCoreClockUpdate();
     Delay_Init();
     USART_Printf_Init(115200);
-    uint8_t led = 0;
     printf("SystemClk:%lu\r\n", SystemCoreClock);
     printf("ChipID:%08lx\r\n", DBGMCU_GetCHIPID());
 
@@ -47,28 +40,26 @@ int main(void) {
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 //    uint32_t p[3] = {10,20,30};
-    TIM2_PWMOut_Init(60-1, 2-1, 10);
-//    TIM2_DMA_Init(DMA1_Channel2, (u32)TIM2_CH2CVR_ADDRESS, (u32)p, 3);
-//
+    TIM2_PWMOut_Init(10-1, 12-1, 0);
+    TIM2_DMA_Init(DMA1_Channel2, (u32)TIM2_CH2CVR_ADDRESS, COLOR_BUFFER_LEN);
+
     TIM_DMACmd(TIM2, TIM_DMA_Update, ENABLE);
     TIM_Cmd(TIM2, ENABLE);
     TIM_CtrlPWMOutputs(TIM2, ENABLE);
-
-    uint8_t color_red[] = {255, 0, 0};
-    WS_Send(color_red, 63);
-
-    while (1) {
-        //闪灯确认工作状态
-        if (led == 1) {
-            led = 0;
-        } else {
-            led = 1;
-        }
-        GPIO_WriteBit(GPIOA, GPIO_Pin_0, led);
-
-        Delay_Ms(100);
+    for (int i = 0; i < Pixel_NUM; i++) {
+        setPixelColor(i, 0, 0, 0);
     }
+    led_example_2();
 
+
+}
+void DMA1_Channel2_IRQHandler(void)
+{
+    if (DMA_GetFlagStatus( DMA1_FLAG_TC2)) {
+        TIM_Cmd( TIM2, DISABLE);
+        DMA_Cmd( DMA1_Channel2, DISABLE);
+        DMA_ClearFlag( DMA1_FLAG_TC2);
+    }
 }
 
 
